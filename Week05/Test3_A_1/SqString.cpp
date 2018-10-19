434b42ba4a48053c66900fe5fbe912213e580231
COCO5666 <zj175@139.com>
2018-10-19 22:52:44 +0800

    COCO5666 Signed-off-by: COCO5666 <zj175@139.com>

--- Week05/Test3_A_1/SqString.cpp
+++ Week05/Test3_A_1/SqString.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-using std::cout;
-using std::endl;
+
+#include "SqString.h"
+
+using namespace std;
 
 void StrAssign(SqString &s, char cstr[])
 {
@@ -12,7 +14,9 @@ void StrAssign(SqString &s, char cstr[])
 
 void DispStr(SqString s)
 {
-	cout << s << endl;
+	for(int i=0; i<s.length; i++)
+		cout << s.data[i];
+	cout << endl;
 }
 
 int StrLength(SqString s)
@@ -60,7 +64,7 @@ SqString RepStr(SqString s, int i, int j, SqString t)
 	int k;
 	SqString str;
 	str.length = 0;
-	if(i<0 || i>s.length || i-1+j >= length-1)
+	if(i<0 || i>s.length || i-1+j > s.length)
 		return str;
 	for(k=0; k<=i-2; k++)
 		str.data[k] = s.data[k];
@@ -72,10 +76,27 @@ SqString RepStr(SqString s, int i, int j, SqString t)
 	return str;
 }
 
-SqString SubStr(SqString s, int i, int j)
+SqString SubStr(SqString s, int i, int j) //subsidiary
 {
 	int k;
 	SqString str;
 	str.length = 0;
-	if(i<=0 || i>length || j<0 || i-1+j > s.length)
+	if(i<=0 || i>s.length || j<0 || i-1+j > s.length)
 		return str;
+	for(k=i-1; k<i-1+j; k++)
+		str.data[k-i+1] = s.data[k];
+	str.length = j;
+	return str;
+}
+
+SqString Concatenate(SqString s, SqString t)
+{
+	int i;
+	SqString str;
+	str.length = s.length + t.length;
+	for(i=0; i<s.length; i++)
+		str.data[i] = s.data[i];
+	for(i=0; i<t.length; i++)
+		str.data[i+s.length] = t.data[i];
+	return str;
+}
--- Week05/Test3_A_1/SqString.h
+++ Week05/Test3_A_1/SqString.h
@@ -12,3 +12,5 @@ int StrLength(SqString s);
 SqString InsStr(SqString s1, int i, SqString s2);
 SqString DelStr(SqString s, int i, int j);
 SqString RepStr(SqString s, int i, int j, SqString t);
+SqString SubStr(SqString s, int i, int j);
+SqString Concatenate(SqString s, SqString t);
