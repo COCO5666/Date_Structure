e9de78f6329bb202bdd218c17e4acfc84862b8ad
COCO5666 <zj175@139.com>
2018-11-06 21:16:01 +0800

    COCO5666

--- 自动提交.bat
+++ 自动提交.bat
@@ -1,9 +1,9 @@
 d:
-cd D:\0COCO\本科\大二上学期\数据结构-习题和实验代码
+cd "D:\0COCO\本科\大二上学期\数据结构-习题和实验代码"
 git remote add origin git@github.com:Date_Structure.git
 git status
 git add --all
 git commit -m "COCO5666"
 git push -u origin master
 start https://github.com/COCO5666?tab=repositories
-set /p a=输入任意键退出程序:
+set /p a=Press any key to quit .:
